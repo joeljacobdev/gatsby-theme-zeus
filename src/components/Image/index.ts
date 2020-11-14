8d10f11c1891d7fed38a8177feaef33803a06987
Joel Jacob <joeljacobdev@gmail.com>
2020-11-14 17:53:37 +0530

    Removed image zoom component

--- src/components/Image/index.ts
+++ src/components/Image/index.ts
@@ -1,4 +1,3 @@
 export { default } from "./Image";
-export { default as ImageZoom } from "./Image.Zoom";
 export { default as ImagePlaceholder } from "./Image.Placeholder";
 export {default as MdxImg } from "./MdxImg"
